92248bfdf66386dcafde6d2816fbcf9580233f3f
Rashad-Muntar <muntartoure@gmail.com>
2021-09-02 13:45:24 +0000

    Activities api call done in Category details

--- src/Containers/CategoryDetails.js
+++ src/Containers/CategoryDetails.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const CategoryDetails = () => {
+  const [activity, setActivity] = useState(null);
   const id = useParams();
   console.log(id.id);
+
+  const getActivitties = async () => {
+    try {
+      const response = await axios.get(`https://stark-hollows-83409.herokuapp.com/api/categories/${id.id}/activities`);
+      const { data } = response;
+      setActivity(data);
+    } catch (error) {
+      return error.message;
+    }
+  };
+
+  useEffect(() => {
+    getActivitties();
+  }, []);
   return (
     <div>
       <h3>Details pages</h3>
