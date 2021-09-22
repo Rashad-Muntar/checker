b00e5facd84a80911733d499d2eafbb0104c9daf
Rashad-Muntar <muntartoure@gmail.com>
2021-09-22 00:10:57 +0000

    Add test packages

--- src/Containers/Navbar.js
+++ src/Containers/Navbar.js
@@ -9,12 +9,15 @@ const Navbar = ({ logoutHandler }) => {
   console.log(user);
 
   return (
-    <nav>
-
-      <div />
-      <div className="content-wrapper">
-        <div className="left"><Link to="/">dayTrack+</Link></div>
-        <div className="right">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link to="/" className="navbar-brand">Checker+</Link>
+        {/* <a className="navbar-brand" href="google.com">Navbar</a> */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="braille icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0" />
           {user.user && user.user.loggedIn === true
             ? (
               <>
@@ -32,6 +35,30 @@ const Navbar = ({ logoutHandler }) => {
         </div>
       </div>
     </nav>
+  // <nav>
+
+  //   <div />
+  //   <div className="content-wrapper">
+  //     <div className="left"><Link to="/">Checker+</Link></div>
+  //     <div className="right">
+  // {user.user && user.user.loggedIn === true
+  //   ? (
+  //     <>
+  //       {}
+  //       <i className="user outline icon" />
+  //       <span className="name">{user.user.username}</span>
+  //       <button type="button" className="logout" onClick={logoutHandler}>Logout</button>
+  //     </>
+  //   ) : (
+  //     <>
+  //       <Link to="/login">Login</Link>
+  //       <Link to="/signup">Signup</Link>
+  //     </>
+  //   )}
+  //     </div>
+  //   </div>
+  // </nav>
+
   );
 };
 
